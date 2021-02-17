073e6461e5e55dfecd7e00a9125e0b9b61d2abac
cpucode <923992029@qq.com>
2021-02-17 12:55:54 +0800

    DynamicFactoryTest__实例工厂

--- README.md
+++ README.md
@@ -19,5 +19,6 @@ Spring个人学习代码
 - [SpringProtoTest__Spring多例模式](cpucode_spring_ioc/src/test/java/com/cpucode/test/SpringProtoTest.java)
 - [SpringMethodTest__初始销毁方法](cpucode_spring_ioc/src/test/java/com/cpucode/test/SpringMethodTest.java)
 - [StaticFactoryTest__静态工厂](cpucode_spring_ioc/src/test/java/com/cpucode/test/StaticFactoryTest.java)
+- [DynamicFactoryTest__实例工厂](cpucode_spring_ioc/src/test/java/com/cpucode/test/DynamicFactoryTest.java)
 
 - [返回文件目录](#文件目录)
---
+++ cpucode_spring_ioc/src/test/java/com/cpucode/test/DynamicFactoryTest.java
@@ -0,0 +1,23 @@
+package com.cpucode.test;
+
+import com.cpucode.dao.UserDao;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author : cpucode
+ * @date : 2021/2/17
+ * @time : 12:51
+ * @github : https://githfub.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class DynamicFactoryTest {
+    @Test
+    public void test1(){
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao userDao = (UserDao) app.getBean("dynamicFactory");
+
+        System.out.println(userDao);
+    }
+}
