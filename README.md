0095229d7caaabe97c8d710fc3ea9eeed880c1fd
cpucode <923992029@qq.com>
2021-02-17 12:08:55 +0800

    SpringTest__Spring单例模式

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Spring个人学习代码
 ## 文件目录
 
 - [UserDaoDemo__第一个Spring](cpucode_spring_ioc/src/main/java/com/cpucode/demo/UserDaoDemo.java)
-
-
+- [SpringTest__Spring单例模式](cpucode_spring_ioc/src/test/java/com/cpucode/test/SpringTest.java)
+- [SpringProtoTest__Spring多例模式](cpucode_spring_ioc/src/test/java/com/cpucode/test/SpringProtoTest.java)
 
 - [返回文件目录](#文件目录)
---
+++ cpucode_spring_ioc/src/test/java/com/cpucode/test/SpringTest.java
@@ -0,0 +1,35 @@
+package com.cpucode.test;
+
+import com.cpucode.dao.UserDao;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * @author : cpucode
+ * @date : 2021/2/17
+ * @time : 11:52
+ * @github : https://githfub.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public class SpringTest {
+
+    /**
+     * 测试scope属性
+     * */
+    @Test
+    public void test1(){
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao userDao = (UserDao) app.getBean("userDaoSingle");
+        UserDao userDao2 = (UserDao) app.getBean("userDaoSingle");
+
+        System.out.println(userDao);
+        System.out.println(userDao2);
+    }
+}
+
+/**
+ * com.cpucode.dao.impl.UserDaoImpl@17579e0f
+ * com.cpucode.dao.impl.UserDaoImpl@17579e0f
+ * */
