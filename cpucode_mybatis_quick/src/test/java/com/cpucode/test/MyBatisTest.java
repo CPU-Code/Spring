c03b63d74482b445df86db4a5066b14bd66e3693
cpuCode <923992029@qq.com>
2021-03-02 20:32:21 +0800

    删除操作

--- cpucode_mybatis_quick/src/test/java/com/cpucode/test/MyBatisTest.java
+++ cpucode_mybatis_quick/src/test/java/com/cpucode/test/MyBatisTest.java
@@ -104,4 +104,26 @@ public class MyBatisTest {
         //释放资源
         sqlSession.close();
     }
+
+    /**
+     * 删除操作
+     */
+    @Test
+    public void test4() throws IOException {
+        //获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session回话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行操作
+        int row = sqlSession.delete("userMapper.delete", 3);
+
+        System.out.println(row);
+        //mybatis执行更新操作  提交事务
+        sqlSession.commit();
+        //释放资源
+        sqlSession.close();
+    }
 }
