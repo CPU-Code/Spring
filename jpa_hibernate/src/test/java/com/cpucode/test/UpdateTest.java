92b95eb39a165e8c6e667fea84fc1c0dc3f4204b
cpuCode <923992029@qq.com>
2021-03-15 15:58:41 +0800

    添加测试注解

--- jpa_hibernate/src/test/java/com/cpucode/test/UpdateTest.java
+++ jpa_hibernate/src/test/java/com/cpucode/test/UpdateTest.java
@@ -3,17 +3,20 @@ package com.cpucode.test;
 import com.cpucode.dao.CustomerDao;
 import com.cpucode.domain.Customer;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
-
- *
  * @author : cpucode
  * @date : 2021/3/15
  * @time : 13:44
  * @github : https://github.com/CPU-Code
  * @csdn : https://blog.csdn.net/qq_44226094
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class UpdateTest {
 
     @Autowired
