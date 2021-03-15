576a4c66bde31b167444b43ba8e5286f4e2d4cd8
cpuCode <923992029@qq.com>
2021-03-15 16:03:50 +0800

    SaveTest__保存

--- jpa_hibernate/README.md
+++ jpa_hibernate/README.md
@@ -3,6 +3,7 @@
 
 - [ ][FindOneTest__查询单一](src/test/java/com/cpucode/test/FindOneTest.java)
 - [ ][UpdateTest__修改](src/test/java/com/cpucode/test/UpdateTest.java)
+- [ ][SaveTest__保存](src/test/java/com/cpucode/test/SaveTest.java)
 
 -----------------
 
---
+++ jpa_hibernate/src/test/java/com/cpucode/test/SaveTest.java
@@ -0,0 +1,35 @@
+package com.cpucode.test;
+
+import com.cpucode.dao.CustomerDao;
+import com.cpucode.domain.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author : cpucode
+ * @date : 2021/3/15
+ * @time : 15:57
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class SaveTest {
+    @Autowired
+    private CustomerDao customerDao;
+
+    /**
+     * 保存客户：调用save(obj)方法
+     */
+    @Test
+    public void test1(){
+        Customer customer = new Customer();
+
+        customer.setCustName("cpu");
+
+        customerDao.save(customer);
+    }
+}
