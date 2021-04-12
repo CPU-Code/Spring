98765245cb4e6b519aeb6aa66f0f8356cbde9627
cpuCode <923992029@qq.com>
2021-04-12 20:54:53 +0800

    自动填充

--- mybatis_plus/src/main/java/com/cpucode/mybatis_plus/entity/User.java
+++ mybatis_plus/src/main/java/com/cpucode/mybatis_plus/entity/User.java
@@ -1,8 +1,12 @@
 package com.cpucode.mybatis_plus.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
@@ -12,4 +16,18 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+
+    /**
+     * create_time
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * update_time
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+
 }
---
+++ mybatis_plus/src/main/java/com/cpucode/mybatis_plus/handler/MyMetaObjectHandler.java
@@ -0,0 +1,36 @@
+package com.cpucode.mybatis_plus.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author : cpucode
+ * @date : 2021/4/12
+ * @time : 20:50
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    /**
+     * mp执行添加操作，这个方法执行
+     * @param metaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+
+    /**
+     * mp执行修改操作，这个方法执行
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+}
