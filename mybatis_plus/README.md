1b73e694a6cf93ff4fb01f40265310bd0e50c311
cpuCode <923992029@qq.com>
2021-04-12 22:41:25 +0800

    SelectByMapTest__条件查询

--- mybatis_plus/README.md
+++ mybatis_plus/README.md
@@ -6,6 +6,7 @@
 - [x] [UpdateTest__修改数据](src/test/java/com/cpucode/mybatis_plus/UpdateTest.java)
 - [x] [OptimisticLockerTest__乐观锁测试](src/test/java/com/cpucode/mybatis_plus/OptimisticLockerTest.java)
 - [x] [SelectIdTest__多个id查询](src/test/java/com/cpucode/mybatis_plus/SelectIdTest.java)
+- [x] [SelectByMapTest__条件查询](src/test/java/com/cpucode/mybatis_plus/SelectByMapTest.java)
 
 -----------------
 
---
+++ mybatis_plus/src/test/java/com/cpucode/mybatis_plus/SelectByMapTest.java
@@ -0,0 +1,39 @@
+package com.cpucode.mybatis_plus;
+
+import com.cpucode.mybatis_plus.entity.User;
+import com.cpucode.mybatis_plus.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author : cpucode
+ * @date : 2021/4/12
+ * @time : 22:39
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@SpringBootTest
+public class SelectByMapTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 测试乐观锁
+     */
+    @Test
+    public void test1() {
+        Map<String, Object> columnMap = new HashMap<>();
+
+        columnMap.put("name", "Jack");
+        columnMap.put("age", 20);
+
+        List<User> users = userMapper.selectByMap(columnMap);
+
+        System.out.println(users);
+    }
+}
